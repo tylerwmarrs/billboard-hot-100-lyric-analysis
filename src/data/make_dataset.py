3b8ee66760b05ecd1a7e365a14420be977145cdb
Tyler Marrs <tylerwmarrs@gmail.com>
2017-02-09 16:55:46 -0600

    add logic to clean lyric files moving from raw data dir to processed

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,54 @@
 # -*- coding: utf-8 -*-
+"""
+make_dataset.py: Walks the raw data dirs and cleans the files placing them in the associated processed directory.
+"""
+
+__author__      = "Tyler Marrs"
+
 import os
-import click
-import logging
-from dotenv import find_dotenv, load_dotenv
+import sys
+
+# Hack to get src dir on module path
+project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+
+if project_dir not in sys.path:
+   sys.path.append(project_dir)
+
+from src import corpus
+import re
+
+tagged = re.compile('\[.*\]')
+produced = re.compile('[p|P]roduced.*')
+
+def clean_lyrics(lyrics):
+    """Cleans the lyrics text."""
+    new_lyrics = tagged.sub('', lyrics)
+    new_lyrics = produced.sub('', new_lyrics)
+    
+    return new_lyrics
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
-
+def main():
+    processed_dir = corpus.processed_data_dir()
+    
+    for raw_dir in corpus.raw_data_dirs():
+        
+        #create new processed dir
+        dir_name = raw_dir.split('/')[-1]
+        new_dir = os.path.join(processed_dir, dir_name)
+        os.makedirs(new_dir, exist_ok=True)
+        
+        for f_name in os.listdir(raw_dir):
+            new_file = os.path.join(new_dir, f_name)
+            
+            # copy file content to new processed file cleaning lyrics as required
+            with open(os.path.join(raw_dir, f_name)) as raw_file:
+                file_content = raw_file.read()
+                with open(new_file, 'wt') as processed_file:
+                    if not f_name == 'songs.csv':                    
+                        file_content = clean_lyrics(file_content)
+                        
+                    processed_file.write(file_content)
+    
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
     main()
